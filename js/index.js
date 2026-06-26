23a93e47cebbfe691c06e1e08495a2d3f39a38d8
umergalin <91934841+umergalin@users.noreply.github.com>
2026-06-26 12:42:59 +0500

    refactor: extract setupEventListeners and handlers

--- js/index.js
+++ js/index.js
@@ -1,40 +1,40 @@
-import { loadAudio, preprocessAudio } from './audio-processor.mjs'
-import { BubbleChart } from './bubble-chart.mjs';
-import { WavePlotter } from './wave-plotter.mjs';
-import { getCSSVar } from './utils.js';
-import { PATHS, GENRES, UI_DEFAULTS } from './config.js';
+import { loadAudio, preprocessAudio } from "./audio-processor.mjs";
+import { BubbleChart } from "./bubble-chart.mjs";
+import { WavePlotter } from "./wave-plotter.mjs";
+import { getCSSVar } from "./utils.js";
+import { PATHS, GENRES, UI_DEFAULTS } from "./config.js";
 
 console.log("hi");
 
-const waveformContainer = document.querySelector('.js-waveform-container')
+const waveformContainer = document.querySelector(".js-waveform-container");
 const wavePlotter = new WavePlotter(waveformContainer);
 
-const bubbleChartContainer = document.querySelector('.bubble-chart');
+const bubbleChartContainer = document.querySelector(".bubble-chart");
 const bubbleChart = new BubbleChart(bubbleChartContainer, GENRES);
 
-const audioFileInput = document.getElementById('audio-file-input');
-const runAnalysisButton = document.querySelector('.js-run-analysis');
+const audioFileInput = document.getElementById("audio-file-input");
+const runAnalysisButton = document.querySelector(".js-run-analysis");
 
-const pageInput = document.querySelector('.page-input');
-const pageResult = document.querySelector('.page-result');
+const pageInput = document.querySelector(".page-input");
+const pageResult = document.querySelector(".page-result");
 
-const contentContainer = document.getElementById('content-container')
-const backToInputButton = document.querySelector('.js-back-trigger');
+const contentContainer = document.getElementById("content-container");
+const backToInputButton = document.querySelector(".js-back-trigger");
 
-const nameOutput = document.querySelector('.metadata .name');
-const authorOutput = document.querySelector('.metadata .author');
+const nameOutput = document.querySelector(".metadata .name");
+const authorOutput = document.querySelector(".metadata .author");
 
-const resultOutput = document.querySelector('.result .genre');
-
-const loader = document.querySelector('.loader');
+const resultOutput = document.querySelector(".result .genre");
+const loader = document.querySelector(".loader");
 
 function displayTrackInfo(file) {
   const fileName = file.name;
 
-  const lastDotIndex = fileName.lastIndexOf('.');
-  const trackTitle = lastDotIndex > 0 ? fileName.substring(0, lastDotIndex) : fileName;
+  const lastDotIndex = fileName.lastIndexOf(".");
+  const trackTitle =
+    lastDotIndex > 0 ? fileName.substring(0, lastDotIndex) : fileName;
 
-  const separatorIndex = trackTitle.indexOf('-');
+  const separatorIndex = trackTitle.indexOf("-");
 
   if (separatorIndex !== -1) {
     authorOutput.textContent = trackTitle.slice(0, separatorIndex).trim();
@@ -48,46 +48,11 @@ function displayResult(genre) {
   resultOutput.textContent = genre.toUpperCase();
 }
 
-audioFileInput.addEventListener('change', () => {
-  const file = audioFileInput.files[0];
-
-  if (!file) {
-    runAnalysisButton.disabled = true;
-    return;
-  }
-
-  const audio = new Audio();
-  const objectUrl = URL.createObjectURL(file);
-  audio.src = objectUrl;
-
-  audio.onloadedmetadata = function () {
-    URL.revokeObjectURL(objectUrl);
-
-    if (audio.duration < 3) {
-      console.log("Файл слишком короткий (минимум 3 секунды)");
-      audioFileInput.value = "";
-      runAnalysisButton.disabled = true;
-    } else {
-      runAnalysisButton.disabled = false;
-    }
-  };
-}) 
-
-
-
-backToInputButton.addEventListener('click', () => {
-  contentContainer.classList.remove('show-result');
-
-  audioFileInput.value = '';
-  runAnalysisButton.disabled = true;
-
-  clearResults();
-})
-
-function clearResults() { // лучше сбрасывать только тогда, когда страница уже уедет за экран
-  authorOutput.textContent = '';
-  nameOutput.textContent = '';
-  resultOutput.textContent = '';
+function clearResults() {
+  // лучше сбрасывать только тогда, когда страница уже уедет за экран
+  authorOutput.textContent = "";
+  nameOutput.textContent = "";
+  resultOutput.textContent = "";
 
   wavePlotter.reset();
   bubbleChart.reset();
@@ -98,13 +63,8 @@ function prepareForProcessing() {
   nameOutput.textContent = UI_DEFAULTS.NAME;
   resultOutput.textContent = UI_DEFAULTS.STATUS;
 
-  loader.classList.remove('hidden');
+  loader.classList.remove("hidden");
 }
-
-function updateWindowSize() {
-  bubbleChart.resize();
-}
-window.onresize = updateWindowSize;
 
 function setupWorker({ onStart, onSegment, onFinal }) {
   const worker = new Worker(PATHS.worker, { type: "module" });
@@ -116,25 +76,16 @@ function setupWorker({ onStart, onSegment, onFinal }) {
     console.error(`Файл: ${event.filename}, Строка: ${event.lineno}`);
   };
 
-  worker.onmessage = function (e) {
-    // Слушаем сообщения из воркера
-    const message = e.data;
-    switch (message.type) {
+  worker.onmessage = function ({ data }) {
+    switch (data.type) {
       case "start":
-        bubbleChart.updateStepSize(message.segmentCount);
-        wavePlotter.setSegmentsCount(message.segmentCount);
+        onStart(data);
         break;
       case "segment":
-        const genre = GENRES[message.genreIndex];
-        wavePlotter.setSegmentColor(
-          message.segmentIndex,
-          getCSSVar(`--${genre.id}-bg`),
-        );
-        bubbleChart.addBubble(message.genreIndex);
+        onSegment(data);
         break;
       case "final":
-        displayResult(GENRES[message.genreIndex].id);
-        loader.classList.add("hidden");
+        onFinal(data);
         break;
     }
   };
@@ -142,36 +93,77 @@ function setupWorker({ onStart, onSegment, onFinal }) {
   return worker;
 }
 
-function setupEventListeners(worker) {
-  runAnalysisButton.addEventListener("click", async () => {
-    if (!audioFileInput.files.length) {
-      alert("Выберите аудиофайл");
-      return;
+function validateAudioDuration(file, { onValid, onInvalid }) {
+  const audio = new Audio();
+  const objectUrl = URL.createObjectURL(file);
+  audio.src = objectUrl;
+
+  audio.onloadedmetadata = function () {
+    URL.revokeObjectURL(objectUrl);
+
+    if (audio.duration < 3) {
+      console.log("Файл слишком короткий (минимум 3 секунды)");
+      onInvalid();
+    } else {
+      onValid();
     }
+  };
+}
 
-    const chosenFile = audioFileInput.files[0];
+function handleFileInputChange() {
+  const file = audioFileInput.files[0];
 
-    clearResults();
-    prepareForProcessing();
+  if (!file) {
+    runAnalysisButton.disabled = true;
+    return;
+  }
 
-    displayTrackInfo(chosenFile);
-
-    contentContainer.classList.add("show-result");
-
-    const audioBuffer = await loadAudio(chosenFile);
-
-    wavePlotter.render(audioBuffer);
-
-    const processedAudioData = await preprocessAudio(audioBuffer);
-
-    worker.postMessage(
-      {
-        type: "runAnalysis",
-        audioData: processedAudioData,
-      },
-      [processedAudioData.buffer],
-    );
+  validateAudioDuration(file, {
+    onValid: () => (runAnalysisButton.disabled = false),
+    onInvalid: () => {
+      audioFileInput.value = "";
+      runAnalysisButton.disabled = true;
+    },
   });
+}
+
+function handleBackToInput() {
+  contentContainer.classList.remove("show-result");
+  audioFileInput.value = "";
+  runAnalysisButton.disabled = true;
+  clearResults();
+}
+
+async function handleRunAnalysis(worker) {
+  const file = audioFileInput.files[0];
+  await startAnalysis(file, worker);
+}
+
+async function startAnalysis(file, worker) {
+  clearResults();
+  prepareForProcessing();
+
+  displayTrackInfo(file);
+  contentContainer.classList.add("show-result");
+
+  const audioBuffer = await loadAudio(file);
+  wavePlotter.render(audioBuffer);
+  const processedAudioData = await preprocessAudio(audioBuffer);
+
+  worker.postMessage(
+    {
+      type: "runAnalysis",
+      audioData: processedAudioData,
+    },
+    [processedAudioData.buffer],
+  );
+}
+
+function setupEventListeners(worker) {
+  audioFileInput.addEventListener("change", handleFileInputChange);
+  backToInputButton.addEventListener("click", handleBackToInput);
+  runAnalysisButton.addEventListener("click", () => handleRunAnalysis(worker));
+  window.addEventListener("resize", () => bubbleChart.resize());
 }
 
 function init() {

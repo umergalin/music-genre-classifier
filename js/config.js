a68803ece6b2b8cf3d31541fc4c7c4f0c90223d1
umergalin <91934841+umergalin@users.noreply.github.com>
2026-06-29 17:57:24 +0500

    refactor: remove duplicated data structures

--- js/config.js
+++ js/config.js
@@ -16,32 +16,6 @@ export const GENRES = [
   { id: "rock", label: "рок", emoji: "🎸" },
 ];
 
-export const GENRES_TRANSLATION = {
-  "blues": "блюз",
-  "classical": "классическая",
-  "country": "кантри",
-  "disco": "диско",
-  "hiphop": "хип-хоп",
-  "jazz": "джаз",
-  "metal": "метал",
-  "pop": "поп",
-  "reggae": "регги",
-  "rock": "рок"
-}
-
-export const GENRES_EMOJIS = {
-  "blues": "🎺",
-  "classical": "🎹",
-  "country": "🤠",
-  "disco": "💿",
-  "hiphop": "🎛️",
-  "jazz": "🎷",
-  "metal": "💀",
-  "pop": "🎤",
-  "reggae": "☮️",
-  "rock": "🎸"
-}
-
 export const UI_DEFAULTS = {
   AUTHOR: 'Неизвестен',
   NAME: 'Неизвестно',
